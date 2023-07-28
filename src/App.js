a4ceff627be20688b526663d29348fe8b32bcbba
nomoto03 <nekomon6030@gmail.com>
2023-07-28 23:45:25 +0900

    state,reducerの定義と追加ボタンの実装

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useReducer } from "react";
 import "./App.css";
 import NewTaskForm from "./components/NewTaskForm";
 import TaskList from "./components/TaskList";
@@ -8,17 +9,62 @@ const STATUSES = {
   DONE: "DONE",
 };
 
-const todos = [
-  { id: 0, title: "朝ごはん", status: STATUSES.DONE },
-  { id: 1, title: "昼ごはん", status: STATUSES.IN_PROGRESS },
-  { id: 2, title: "夜ごはん", status: STATUSES.TODO },
-];
+const todosReducer = (state, action) => {
+  switch (action.type) {
+    case "add":
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          title: action.payload.title,
+          status: STATUSES.TODO,
+        },
+      ];
+    case "update":
+      const newTodos = [...state];
+      newTodos.map((todo) =>
+        todo.id === action.payload.id
+          ? {
+              id: action.payload.id,
+              title: action.payload.title,
+              status: action.payload.status,
+            }
+          : todo
+      );
+      return [...state];
+    case "delete":
+      return [...state].filter((todo) => todo.id !== action.payload.id);
+    default:
+      throw Error("Unknown action:" + action.type);
+  }
+};
 
 function App() {
+  const initialTodos = [
+    { id: 0, title: "朝ごはん", status: STATUSES.DONE },
+    { id: 1, title: "昼ごはん", status: STATUSES.IN_PROGRESS },
+    { id: 2, title: "夜ごはん", status: STATUSES.TODO },
+  ];
+  const [latestTodoId, setLatestTodoId] = useState(initialTodos.length);
+  const [newTask, setNewTask] = useState("");
+  const [todos, dispatchTodos] = useReducer(todosReducer, initialTodos);
+  // ↓ここで定義するのか？
+  const handleAddTodo = () => {
+    dispatchTodos({
+      type: "add",
+      payload: { id: latestTodoId, title: newTask },
+    });
+    setLatestTodoId(latestTodoId + 1);
+  };
   return (
     <div className="App">
       <h1>TODOリスト</h1>
-      <NewTaskForm />
+      {/* ↓compornentに渡すpropsはこれで良い？ */}
+      <NewTaskForm
+        newTask={newTask}
+        setNewTask={setNewTask}
+        handleAddTodo={handleAddTodo}
+      />
       <TaskList todos={todos} />
     </div>
   );
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-const NewTaskForm = () => {
+const NewTaskForm = ({ newTask, setNewTask, handleAddTodo }) => {
   return (
     <div>
       <h2>新規タスク</h2>
-      <input type="text" />
-      <button>追加</button>
+      <input
+        type="text"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>追加</button>
     </div>
   );
 };
