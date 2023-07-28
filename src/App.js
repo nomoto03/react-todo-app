e2150466437a1e70df805266d9c75c376409ebe5
nomoto03 <nekomon6030@gmail.com>
2023-07-28 19:58:13 +0900

    見た目だけ作成

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const STATUSES = {
+    TODO: "TODO",
+    IN_PROGRESS: "IN_PROGRESS",
+    DONE: "DONE",
+  };
+  const todos = [
+    { id: 0, title: "朝ごはん", status: STATUSES.DONE },
+    { id: 1, title: "昼ごはん", status: STATUSES.IN_PROGRESS },
+    { id: 2, title: "夜ごはん", status: STATUSES.TODO },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODOリスト</h1>
+      <div>
+        <h2>新規タスク</h2>
+        <input type="text" />
+        <button>追加</button>
+      </div>
+      <div>
+        <h2>タスク一覧</h2>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {todo.title}
+              <button>完了</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
